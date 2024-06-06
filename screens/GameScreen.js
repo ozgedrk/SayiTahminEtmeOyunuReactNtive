fab1536190be559ed120c41af924e8428dbd8516
ozgedrk <121420646+ozgedrk@users.noreply.github.com>
2024-06-06 23:09:22 +0300

    GameScreen Updates

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,11 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import Title from '../components/Title';
+import ComputerNumber from '../components/ComputerNumber';
 
-export default function GameScreen() {
+export default function GameScreen({ userNumber }) {
+  const initialGuess = generateNumber(1, 100 ,userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess)
+
+  function generateNumber(min, max, exclude) {
+    const randomNumber = Math.floor(Math.random() * (max-min)) + min;
+
+    if(randomNumber === exclude)
+    {
+      return randomNumber(min,max,exclude);
+    }
+    else
+    {
+      return randomNumber;
+    }
+  }
   return (
     <View style={styles.container} >
       <Title>Computers Guess</Title>
+      <ComputerNumber>{currentGuess}</ComputerNumber>
       <View>
         <Text>Is the number above or below?</Text>
       </View>
